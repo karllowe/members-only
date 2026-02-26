bb7824e88e425d4a7c8f7c13a71e9b770621ae53
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-26 14:40:45 +0000

    added password confirmation requirement and validatioin

--- controllers/validators.js
+++ controllers/validators.js
@@ -18,17 +18,31 @@ const signupValidationRules = [
             if (user) {
                 throw new Error("username already in use");
             };
+            return true
         }),
     body("password")
-        .isLength({min: 6}).withMessage("Password must be at least 8 characters long")
+        .notEmpty().withMessage("Please enter a password")
+        .isLength({min: 6}).withMessage("Password must be at least 8 characters long"),
+    body("passwordConfirmation")
+        .notEmpty().withMessage("Please confirm your password")
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error("Passwords do not match")
+            }
+            return true
+        })
 ];
 
 const handleValidationErrors = (viewName) => (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+        const safeValues = {...req.body};
+        delete safeValues.password;
+        delete safeValues.passwordConfirmation;
+
         return res.status(400).render(viewName, {
             errors: errors.array(),
-            values: req.body
+            values: safeValues
         });
     }
     next();
